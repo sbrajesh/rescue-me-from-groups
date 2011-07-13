e316cb540d1df79c40aab7141ec16006ed820bca
Brajesh Singh <brajesh@cosmiccoders.com>
2011-07-14 00:01:46 +0530

    Update for bp 1.2.9

--- rescue-me-from-groups.php
+++ rescue-me-from-groups.php
@@ -2,33 +2,14 @@
 /*
 Plugin Name: Recume me from Groups
 Author: Brajesh Singh
-Version: 1.0
+Version: 1.1
 description: It heals the broken soul of site users and rescues them from auto joining the public groups while participating in the forum discussion
-*/
+ * IN BuddyPress 1.2.9, it is a built in thing, we just need to define BP_DISABLE_AUTO_GROUP_JOIN to false
+ */
 
-add_action("check_admin_referer","bpdev_change_group_status_admin",10,2);
-function bpdev_change_group_status_admin($action,$res){
-	if($action=="bp_forums_new_topic")
-	bpdev_change_group_status();
-}
-
-function bpdev_change_group_status(){
-global $bp;
-/* this is a bad hack, but there is no workaround in bp 1.2.3*/
-	if($bp->groups->current_group->status=='public'){
-	$bp->groups->current_group->status_bkp=$bp->groups->current_group->status;//save current status
-	$bp->groups->current_group->status ="something";// change to arbitrary status(we are doing it to make the joining condition false)
-}
-	
-}
-
-add_action("groups_new_forum_topic","bpdev_revert_group_status");
-/*when the topic is created, revert back the status*/
-function bpdev_revert_group_status(){
-global $bp;
-if(!empty($bp->groups->current_group->status_bkp))
-$bp->groups->current_group->status=$bp->groups->current_group->status_bkp;
-//$bp->groups->current_group->status ="something";// ="something";
-
+add_action("groups_setup_globals","rescue_from_autojoin");
+function rescue_from_autojoin(){
+    global $bp;
+    $bp->groups->auto_join=false;
 }
 ?>
